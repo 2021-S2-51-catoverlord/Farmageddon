bda5e2f1433273e481d4882af32d3c7fbe576b12
roxy-codes <61820074+roxy-codes@users.noreply.github.com>
2021-08-24 09:52:41 +1200

    Comments

--- Assets/Scenes/Common/Scripts/HealthBar.cs
+++ Assets/Scenes/Common/Scripts/HealthBar.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/**
+* Used for th health bar.
+*/
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
 
+    //Set max health
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
     }
-
+    
+    //Set current health
     public void SetHealth(int health)
     {
         slider.value = health;
