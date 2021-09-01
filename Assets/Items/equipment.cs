e06de99411394e07f1a4d18429283201b470f601
roxy-codes <roxeh.cakes@gmail.com>
2021-09-01 14:52:12 +1200

    Items Scripts

--- Assets/Items/equipment.cs
+++ Assets/Items/equipment.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
 public class Equipment : Item
 {
     public Equipment(string itemName, int ID, string description) : base(itemName, ID, description)
--- Assets/Items/item.cs
+++ Assets/Items/item.cs
@@ -2,37 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
 public class Item : ScriptableObject
 {
-    public string itemName;
-    public int ID;
-    public string description;
-    public int price;
-    internal Sprite icon;
-
-    [SerializeField]
-    public Sprite itemArt
-    {
-        get
-        {
-            return itemArt;
-        }
-    }
-
-    protected InventorySlotScript slot
-    {
-        get
-        {
-            return slot;
-        }
-
-        set
-        {
-            slot = value;
-        }
-    }
-
+    public string itemName = "New Item";
+    public int ID = 0;
+    public string description = "Add Description";
+    public int price = 0;
+    public Sprite icon = null;
 
     public Item(string itemName, int ID, string description)
     {
