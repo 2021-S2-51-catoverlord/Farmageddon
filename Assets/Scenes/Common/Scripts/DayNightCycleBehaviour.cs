7381c969be03eb9105eaf431f2e09861ac0f9184
CrowBarJonns <macalite@flashgiver.com>
2021-09-03 10:01:07 +1200

    Seasonal Variance Implemented

--- Assets/Scenes/Common/Scripts/DayNightCycleBehaviour.cs
+++ Assets/Scenes/Common/Scripts/DayNightCycleBehaviour.cs
@@ -13,8 +13,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.Universal;
+using UnityEngine.Tilemaps;
+
+// Seasons, in chunks of 3 months
+public enum Season { UNDEFINED, SPRIMMER, SUMTUMN, AUNTER, WINTING };
+
+// Months, each chunk of three corresponds to the above seasons
+public enum MonthName { UNDEFINED, Janril, Febrarch, Maruary, Aprember, Mane, Junember, Julober, Augember, Septuary, Octobust, Novay, Decly };
 
 public class DayNightCycleBehaviour : MonoBehaviour
 {
@@ -22,6 +30,9 @@ public class DayNightCycleBehaviour : MonoBehaviour
     public float gameDayLength = 720.0f;
 
     public float initTime;
+    public int initDay;
+    public int initMonth;
+    public int initYear;
 
     Gradient gradient;
     GradientColorKey[] colorKey;
@@ -32,11 +43,27 @@ public class DayNightCycleBehaviour : MonoBehaviour
     // Midnight Light colour - Dark / muted colours, emphasis on blue tones
     public Color nightColor = new Color(0.25f, 0.25f, 0.6f);
     // Global light source
-    private Light2D light2D;
+    private Light2D timelight;
+
+    public Tilemap[] tilemaps; 
+
+    public Season season;
+    public MonthName month;
+    public int monthLength;
+    private int yearLength;
+
+    private Gradient seasonGradient;
+    private GradientColorKey[] seasonKey;
+    private GradientAlphaKey[] seasonalAlpha;
+    public Color warmColor = new Color(1.0f, 0.90f, 0.90f);
+    public Color neutralColor = new Color(0.90f, 1.0f, 0.90f);
+    public Color coolColor = new Color(0.90f, 0.90f, 1.0f);
 
     public float timeElapsed { get; private set; }
 
     public int dayCount { get; private set; }
+    private int totalDayCount;
+    public int yearCount { get; private set; }
 
     public float localTimeElapsed { get; private set; }
 
@@ -48,7 +75,36 @@ public class DayNightCycleBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        localTimeElapsed += initTime;
+        yearLength = (Enum.GetNames(typeof(MonthName)).Length - 1) * monthLength;
+        if (initTime != 0)
+        {
+            localTimeElapsed += initTime;
+        } else
+        {
+            localTimeElapsed = 0;
+        }
+
+        if (initDay != 0)
+        { 
+            dayCount += initDay;
+        } else
+        {
+            dayCount = 0;
+        }
+
+        if (initMonth != 0)
+        {
+            month = (MonthName)initMonth;
+        } else
+        {
+            month = MonthName.Janril;
+            season = Season.SPRIMMER;
+        }
+
+        if(monthLength < 1)
+        {
+            monthLength = 30;
+        }
 
         if ((float)Math.Round((double)(localTimeElapsed / gameDayLength), 2) < 0.25 || (float)Math.Round((double)(localTimeElapsed / gameDayLength), 2) >= 0.75)
         {
@@ -59,7 +115,7 @@ public class DayNightCycleBehaviour : MonoBehaviour
         }
 
 
-            light2D = GetComponent<Light2D>();
+        timelight = GetComponent<Light2D>();
 
         // Populate the color keys at the relative time 0 and 1 (0 and 100%)
         gradient = new Gradient();
@@ -79,6 +135,26 @@ public class DayNightCycleBehaviour : MonoBehaviour
         alphaKey[1].time = 1.0f;
 
         gradient.SetKeys(colorKey, alphaKey);
+
+        seasonGradient = new Gradient();
+        seasonKey = new GradientColorKey[5];
+
+        seasonKey[0].color = neutralColor;
+        seasonKey[0].time = 0.0f;
+        seasonKey[1].color = warmColor;
+        seasonKey[1].time = 0.25f;
+        seasonKey[2].color = neutralColor;
+        seasonKey[2].time = 0.5f;
+        seasonKey[3].color = coolColor;
+        seasonKey[3].time = 0.75f;
+        seasonKey[4].color = neutralColor;
+        seasonKey[4].time = 1.0f;
+
+        seasonalAlpha = new GradientAlphaKey[1];
+        seasonalAlpha[0].alpha = 1.0f;
+        seasonalAlpha[0].time = 1.0f;
+
+        seasonGradient.SetKeys(seasonKey, seasonalAlpha);
 
     }
 
@@ -103,12 +179,67 @@ public class DayNightCycleBehaviour : MonoBehaviour
             isDay = false;
         }
 
-        light2D.color = gradient.Evaluate(timeRatio);
+        timelight.color = gradient.Evaluate(timeRatio);
+
 
         if (localTimeElapsed >= gameDayLength)
         {
-            dayCount += 1;
+            dayCount++;
+            totalDayCount++;
+
+            float g = (float)totalDayCount / (float)yearLength;
+
+            foreach (Tilemap t in tilemaps)
+            {
+                t.color = seasonGradient.Evaluate(g);
+            }
+
             localTimeElapsed = 0;
+        }
+        if (dayCount == monthLength)
+        {
+            int m = (int)month;
+            m++;
+
+            m = MonthIncrease(m);
+
+            month = (MonthName)m;
+
+            dayCount = 0;
+        }
+        if ((int)month == 1 || (int)month == 2 || (int)month == 3)
+        {
+            season = Season.SPRIMMER;
+        } else if ((int)month == 4 || (int)month == 5 || (int)month == 6)
+        {
+            season = Season.SUMTUMN;
+        } else if ((int)month == 7 || (int)month == 8 || (int)month == 9 )
+        {
+            season = Season.AUNTER;
+        } else if ((int)month == 10 || (int)month == 11 || (int)month == 12)
+        {
+            season = Season.WINTING;
+        } else
+        {
+            season = Season.SPRIMMER;
+        }
+
+        if(totalDayCount >= yearLength)
+        {
+            yearLength++;
+            totalDayCount = 0;
+        }
+    }
+
+    int MonthIncrease(int m)
+    {
+        if ((int)month == 12)
+        {
+            return 1;
+        }
+        else
+        {
+            return m;
         }
     }
 }
