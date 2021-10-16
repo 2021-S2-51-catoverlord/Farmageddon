e97cefc1c343d8b93a091d272920f82274815f59
Kohjen <33144900+Kohjen@users.noreply.github.com>
2021-10-17 09:55:37 +1300

    Squashed commit of the following:
    commit 8a7bbeb0a86b1031f202322429186cf6cb0da6bc
    Author: Amos Foong <cenzltd2011@gmail.com>
    Date:   Thu Oct 14 02:26:09 2021 +1300
    
        Removed Unused Script and Folder
    
        Cleaning before merge/cherry-pick.
    
    commit eb749c7ca4bfa26751864e4173a31fb96cb6feec
    Author: Amos Foong <cenzltd2011@gmail.com>
    Date:   Mon Oct 4 01:32:59 2021 +1300
    
        Background Music Feature
    
        -MainMenu Scene
        --Added MenuMusic gameobject that will play some fitting menu background music. Playlist is configured to only 5 tracks and this object will be destroyed after scene transition.
    
        -GameMap Scene
        --Added a MusicPlayer gameobject which loads some soundtrack from the resources folder and plays it.
        --The MusicPlayer gameobject is set up to be a singleton so that during scene transition, the music continues on and doesn't restart.
    
        -MusicController Script
        --Method to set up a singleton instance (if needed).
        --Method to ensure that there is only 1 audio listener in the current scene.
        --Method to load game soundtrack from the resources folder (if user has not specify tracks to play).
        --Method to shuffle playlist.
        --Method to stop current track playing.
    
    commit f31a8f6bae860a2cd26ddc8016bcd10ba631d336
    Author: Amos Foong <cenzltd2011@gmail.com>
    Date:   Sun Oct 3 22:48:58 2021 +1300
    
        Added Sound Tracks
    
        -MP3 files
        --Imported some Stardew Valley original soundtracks to the resource folder.
        --Optimised each audio clip settings for a music file.
    
        -MusicPlayer GameObject
        --Added an audio source to this gameobject so it will act as a music player.

---
+++ Assets/Scripts/Audio/MusicController.cs
@@ -0,0 +1,120 @@
+using System.Collections; 
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private static MusicController instance;
+    protected AudioSource audioSrc;
+    private Coroutine autoplay;
+
+    public bool IsSingleton;
+    public Object[] BGMusicPlaylist; // Allows a user's customisation of a music playlist for different scenes.
+
+    public AudioSource AudioSrc { get; set; }
+
+    private void Awake()
+    {
+        ConfigSingleton();
+
+        LoadResources();
+    }
+
+    public void Start()
+    {
+        CheckAudioListener();
+        AudioSrc = GetComponent<AudioSource>();
+        AudioSrc.loop = false;
+        AudioSrc.Play();
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if(!AudioSrc.isPlaying)
+        {
+            autoplay = StartCoroutine(PlayRandomTrack()); // Begin to play a random track.
+        }
+    }
+
+    /// <summary>
+    /// Method to ensure there is only one audio listener throughout the 
+    /// scene. Destroys any surplus.
+    /// </summary>
+    private void CheckAudioListener()
+    {
+        AudioListener[] audioListeners = FindObjectsOfType<AudioListener>();
+
+        // Ignore the first AudioListener in the array.
+        if(audioListeners.Length > 1) 
+        {
+            for(int i = 1; i < audioListeners.Length; i++)
+            {                
+                DestroyImmediate(audioListeners[i]); // Remove other audio listeners.
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method to determine whether to set this object up as a singleton 
+    /// or not depending on the boolean set by user.
+    /// </summary>
+    private void ConfigSingleton()
+    {
+        if(IsSingleton)
+        {
+            // Apply singleton design pattern.
+            DontDestroyOnLoad(this);
+
+            if(instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method to auto-load all music from the Resource-->Music folder if 
+    /// no specific music had been specified by the user in the Inspector.
+    /// </summary>
+    private void LoadResources()
+    {
+        if(BGMusicPlaylist == null || BGMusicPlaylist.Length == 0)
+        {
+            // Load all music files in the Assets-->Resources-->Music folder into BGMusicPlaylist.
+            BGMusicPlaylist = Resources.LoadAll("Music", typeof(AudioClip));
+        }
+
+        if(AudioSrc != null)
+        {
+            // Initialise the first file as the clip to be played.
+            AudioSrc.clip = BGMusicPlaylist[0] as AudioClip;
+        }
+    }
+
+    /// <summary>
+    /// Shuffles a random track from the loaded list into the Music Player.
+    /// </summary>
+    protected IEnumerator PlayRandomTrack()
+    {
+        AudioSrc.clip = BGMusicPlaylist[Random.Range(0, BGMusicPlaylist.Length)] as AudioClip; // Load a random track.
+        AudioSrc.Play();
+        yield return new WaitUntil(() => !AudioSrc.isPlaying); // Wait until the audio source stops playing.
+    }
+
+    /// <summary>
+    /// Method to skip the current track playing. Will cause the  
+    /// computer to select another track via Update().
+    /// </summary>
+    public void SkipTrack()
+    {
+        if(autoplay != null)
+        {
+            StopCoroutine(autoplay); // Stop the autoplay coroutine.
+            AudioSrc.Stop(); // Stop playing the current track.
+        }
+    }
+}
