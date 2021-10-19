6bfba1e4deee3630430b81d9b96449f6328ec10c
roxy-codes <roxeh.cakes@gmail.com>
2021-10-19 18:19:09 +1300

    Revert "Level Up Test"
    This reverts commit a3f19e168e82cec8523d369411a23140e4a79b12.

--- Assets/Scripts/Player/LevelSystem.cs
+++ Assets/Scripts/Player/LevelSystem.cs
@@ -72,6 +72,13 @@ public class LevelSystem : MonoBehaviour
         experience += exp;
     }
 
+    public void TestConstruct(int level, int experience, int expToNxtLvl)
+    {
+        this.level = level;
+        this.experience = experience;
+        this.experienceToNextLevel = expToNxtLvl;
+    }
+
     public bool LevelledUp()
     {
         if(experience >= experienceToNextLevel)
--- Assets/Tests/EditmodeTests/LevelUpTest.cs
+++ Assets/Tests/EditmodeTests/LevelUpTest.cs
@@ -3,18 +3,20 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class LevelTest
+public class LevelUpTest
 {
     [UnityTest]
-    public IEnumerator LevelUpTest()
+    public IEnumerator LevelIncreaseTest()
     {
         var temp = new GameObject().AddComponent<LevelSystem>();
 
-        temp.level = 1;
-        temp.experience = 10;
-        temp.experienceToNextLevel = 32;
+        //temp.level = 1;
+        //temp.experience = 10;
+        //temp.experienceToNextLevel = 32;
+        temp.TestConstruct(1, 10, 32);
 
         temp.GainEXP(30);
+
 
         yield return null;
 
