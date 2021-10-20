1648c40f4d4d5f8ee782ad41c38b2f2e7d642794
Amos Foong <cenzltd2011@gmail.com>
2021-10-21 07:53:21 +1300

    Copy-and-paste "merge" harvest-test branch into save-load
    -Copy and pasted essential files from harvest-test branch for resource generation and resource-to-material conversion.
    
    Co-Authored-By: supercoolcharles <80949791+supercoolcharles@users.noreply.github.com>
    Co-Authored-By: Kohjen <33144900+Kohjen@users.noreply.github.com>

---
+++ Assets/Scripts/ResourceSystem/ResourceField.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceField : MonoBehaviour
+{
+    //markers for resource zone
+    [SerializeField]
+    private Transform pointA;
+    [SerializeField]
+    private Transform pointB;
+
+    private float areaWidth; //relates to X value
+    private float areaHeight; //relates to Y value
+
+    private DayNightCycleBehaviour worldClock;
+
+    private int resourcesGathered = 0;
+    [SerializeField]
+    [Range(1, 100)]
+    private int startPopulateAmount = 20;
+    [SerializeField]
+    private GameObject[] spawnList;
+    private int spawnedResources = 0;
+    [SerializeField]
+    private int maxResources = 30;
+
+    private void Start()
+    {
+        worldClock = Object.FindObjectOfType<DayNightCycleBehaviour>();
+        CalcSpawnArea();
+        PopulateField(startPopulateAmount);
+    }
+
+    private void Update()
+    {
+        if((worldClock.time == 1 || worldClock.time == 0) && !(spawnedResources >= maxResources))
+        {
+            Debug.Log("canSpawn");
+            PopulateField(resourcesGathered + Random.Range(0, 3));
+            resourcesGathered = 0;
+        }
+    }
+
+    private void PopulateField(int amount)
+    {
+        for(int i = 0; i < amount; i++)
+        {
+            GenerateResourceNode();
+            spawnedResources++;
+            Debug.Log("new resrouce spawned, total resources spawned" + spawnedResources);
+        }
+    }
+
+    private void GenerateResourceNode()
+    {
+        Instantiate(spawnList[Random.Range(0, spawnList.Length)], GenerateResourcePos(), Quaternion.identity);
+    }
+
+    private void CalcSpawnArea()
+    {
+        float areaWidth = Mathf.Abs(pointA.localPosition.x) + Mathf.Abs(pointB.localPosition.x);
+        this.areaWidth = areaWidth;
+        float areaHeight = Mathf.Abs(pointA.localPosition.y) + Mathf.Abs(pointB.localPosition.y);
+        this.areaHeight = areaHeight;
+    }
+
+    private Vector3 GenerateResourcePos()
+    {
+        float newHeight = pointA.position.y - Random.Range(0, areaHeight);
+        float newWidth = pointA.position.x + Random.Range(0, areaWidth);
+        return new Vector3(newWidth, newHeight, -1);
+    }
+
+
+    //count how many resources have been collected
+    public void ResourceHarvested()
+    {
+        resourcesGathered++;
+        spawnedResources--;
+    }
+}
